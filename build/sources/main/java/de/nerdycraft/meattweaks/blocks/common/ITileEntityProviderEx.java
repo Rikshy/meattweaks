49932b25bb5e3a17933c8c0b585cb1ce03217776
Shyrik <r.wesenberg@gmail.com>
2018-09-08 09:01:00 +0200

    git

--- build/sources/main/java/de/nerdycraft/meattweaks/blocks/common/ITileEntityProviderEx.java
+++
@@ -1,9 +0,0 @@
-package de.nerdycraft.meattweaks.blocks.common;
-
-import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.tileentity.TileEntity;
-
-public interface ITileEntityProviderEx extends ITileEntityProvider {
-
-	public Class<? extends TileEntity> getTileEntityClass();
-}
--- build/sources/main/java/de/nerdycraft/meattweaks/blocks/common/IVariant.java
+++
@@ -1,8 +0,0 @@
-package de.nerdycraft.meattweaks.blocks.common;
-
-import net.minecraft.util.IStringSerializable;
-
-public interface IVariant extends IStringSerializable {
-
-	int getMeta();
-}
